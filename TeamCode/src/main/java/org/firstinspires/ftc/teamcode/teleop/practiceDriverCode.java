83070ec7f3224dc0f8f5e06e2e24fbaa4244d5e9
kots727 <90489449+kots727@users.noreply.github.com>
2022-10-09 14:24:34 -0700

    hj

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/practiceDriverCode.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.arcrobotics.ftclib.gamepad.ButtonReader;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.KeyReader;
+import com.arcrobotics.ftclib.gamepad.ToggleButtonReader;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.ground.GroundIntake;
+import org.firstinspires.ftc.teamcode.transfer.Intake;
+import org.firstinspires.ftc.teamcode.transfer.vfourb;
+
+@TeleOp
+public class practiceDriverCode extends LinearOpMode {
+    Robot robot;
+    Intake intake;
+    //Slides slides;
+    vfourb fourbar;
+    GroundIntake groundIntake;
+    // Turret turret;
+    GamepadEx primary;
+    GamepadEx secondary;
+    KeyReader[] keyReaders;
+    TriggerReader intakeButton, ninjaMode, reset, deposit;
+    ButtonReader liftHigh, liftMedium, liftLow, junction, align;
+    ToggleButtonReader activeGround;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new Robot(hardwareMap);
+        primary = new GamepadEx(gamepad1);
+        secondary = new GamepadEx(gamepad2);
+        intake = robot.intake;
+      //  slides = robot.slides;
+        fourbar = robot.fourbar;
+        groundIntake = robot.groundIntake;
+      //  turret = robot.turret;
+        keyReaders = new KeyReader[]{
+                intakeButton = new TriggerReader(primary, GamepadKeys.Trigger.RIGHT_TRIGGER),
+                ninjaMode = new TriggerReader(primary, GamepadKeys.Trigger.LEFT_TRIGGER),
+                liftHigh = new ButtonReader(secondary, GamepadKeys.Button.LEFT_BUMPER),
+                liftMedium = new ButtonReader(secondary, GamepadKeys.Button.DPAD_UP),
+                liftLow = new ButtonReader(secondary, GamepadKeys.Button.DPAD_LEFT),
+                junction = new ButtonReader(secondary, GamepadKeys.Button.DPAD_DOWN),
+                deposit = new TriggerReader(secondary, GamepadKeys.Trigger.RIGHT_TRIGGER),
+                align = new ButtonReader(secondary, GamepadKeys.Button.RIGHT_BUMPER),
+                reset = new TriggerReader(secondary, GamepadKeys.Trigger.LEFT_TRIGGER),
+                activeGround = new ToggleButtonReader(secondary, GamepadKeys.Button.A),
+        };
+        waitForStart();
+    //    slides.setState(Slides.State.INTAKE);
+        fourbar.setState(vfourb.State.PRIMED);
+        while (opModeIsActive()) {
+            robot.update();
+            for (KeyReader reader : keyReaders) {
+                reader.readValue();
+            }
+                if (ninjaMode.isDown()) {
+                    robot.setWeightedDrivePower(
+                            new Pose2d(
+                                    -gamepad1.left_stick_y * 0.5,
+                                    gamepad1.left_stick_x * 0.5,
+                                    -gamepad1.right_stick_x * 0.5
+                            )
+                    );
+                } else robot.setWeightedDrivePower(
+                        new Pose2d(
+                                -gamepad1.left_stick_y,
+                                gamepad1.left_stick_x,
+                                -gamepad1.right_stick_x
+                        )
+                );
+                if (intakeButton.isDown()) {
+                    intake.setState(Intake.State.INTAKING);
+                    fourbar.setState(vfourb.State.INTAKE_POSITION);
+                }
+           else if (deposit.isDown()) {
+                intake.setState(Intake.State.DEPOSITING);
+            }
+                else{
+                    intake.setState(Intake.State.OFF);
+                }
+                if (liftHigh.wasJustPressed()) {
+                    //slides.setState(Slides.State.HIGH);
+                    fourbar.setState(vfourb.State.DEPOSIT_POSITION);
+                }
+                /*
+                if (liftMedium.wasJustPressed()) {
+                    slides.setState(Slides.State.MID);
+                    fourbar.setState(vfourb.State.DEPOSIT_POSITION);
+                }
+                if (liftLow.wasJustPressed()) {
+                    slides.setState(Slides.State.LOW);
+                    fourbar.setState(vfourb.State.DEPOSIT_POSITION);
+                }
+                if (junction.wasJustPressed()) {
+                    slides.setState(Slides.State.GROUND);
+                    fourbar.setState(vfourb.State.DEPOSIT_POSITION);
+                }
+                if (align.wasJustPressed()) {
+                    turret.setState(Turret.State.ALIGNING);
+                }*/
+
+                if (activeGround.isDown()) {
+                    groundIntake.setState(GroundIntake.State.INTAKING);
+                } else {
+                    groundIntake.setState(GroundIntake.State.OFF);
+                }
+                telemetry.addData("gi state", groundIntake.getState());
+                telemetry.addData("fesoijfesioj",fourbar.getState());
+telemetry.update();
+                if (reset.wasJustPressed()) {
+             //       turret.setState(Turret.State.RESET);
+               //     slides.setState(Slides.State.INTAKE);
+                    fourbar.setState(vfourb.State.PRIMED);
+                 //   intake.setState(Intake.State.OFF);
+                }
+            }
+        }
+    }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/transfer/vfourb.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.transfer;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class vfourb
+{
+    //temporary values
+    static final double INTAKE_POSITION=1;
+    static final double DEPOSIT_POSITION=-.6;
+    static final double PRIMED = .8;
+
+    Servo Running;
+    Servo Supporting;
+    public State state;
+    public enum State
+    {
+        INTAKE_POSITION,DEPOSIT_POSITION, PRIMED
+    }
+
+    public vfourb(HardwareMap hardwareMap)
+    {
+        Running = hardwareMap.get(Servo.class, "v4brun");
+        Supporting = hardwareMap.get(Servo.class, "v4bsup");
+        setState(State.INTAKE_POSITION);
+    }
+
+    public void update()
+    {
+        switch(state)
+        {
+            case INTAKE_POSITION:
+                Running.setPosition(INTAKE_POSITION);
+                Supporting.setPosition(-INTAKE_POSITION);
+            case DEPOSIT_POSITION:
+                Running.setPosition(DEPOSIT_POSITION);
+                Supporting.setPosition(-DEPOSIT_POSITION);
+            case PRIMED:
+                Running.setPosition(PRIMED);
+                Supporting.setPosition(-PRIMED);
+        }
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state)
+    {
+        this.state = state;
+        update();
+    }
+
+}
